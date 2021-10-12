ad5e4c9b5f2a97bf70da9aceae7402e0eb0e042f
Patrick <Patrickbjerre@outlook.dk>
2021-10-12 13:33:47 +0200

    a fresh start

--- 1.Iteration/src/Main.java
+++ 1.Iteration/src/Main.java
@@ -4,10 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Game s = new Game();
         s.play();
-        d.play();
-        // Jeg vil gerne have en lur
     }
-    //test kommentar
-    //endnu en test kommentar
+
 }
-//test
