06d34e4e9383180b1af144af8fae5b796ea5af52
Patrick <Patrickbjerre@outlook.dk>
2021-10-19 13:31:07 +0200

    Now with all Class Files

---
+++ 1.Iteration/src/Interactables.java
@@ -0,0 +1,8 @@
+public abstract class Interactables {
+
+    private String name;
+    private String ShortDescription;
+
+    public abstract String getName();
+    public abstract String getShortDescription();
+}
