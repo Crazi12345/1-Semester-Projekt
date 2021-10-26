416b9cca07fb436ce8131b6ba3b158ac0fc9f268
Patrick <Patrickbjerre@outlook.dk>
2021-10-26 12:56:38 +0200

    Added Steve

--- 1.Iteration/src/Level.java
+++ 1.Iteration/src/Level.java
@@ -16,28 +16,33 @@ public class Level {
     }
     public Room createRooms()
     {
-        Room outside, theatre, pub, lab, office;
+        Room Village_center, Farm, Market, Home, Field;
+        Item Fish = new Item("fish",69,"Smells like teen spirit");
+        NPC Steve = new NPC("Steve","Fat and looks like a nerd",Fish,
+                "His fingers are full of cheetoo dust and he smells fishy");
+        ArrayList<NPC> npcs= new ArrayList<NPC>();
+        npcs.add(Steve);
+        Village_center = new Room(1,"in the village center",npcs,new ArrayList<InanimateObjects>());
+        Farm = new Room(2,"at a farm", new ArrayList<NPC>(),new ArrayList<InanimateObjects>());
+        Market = new Room(3,"at the market", new ArrayList<NPC>(),new ArrayList<InanimateObjects>());
+        Home = new Room(4,"at your home", new ArrayList<NPC>(),new ArrayList<InanimateObjects>());
+        Field = new Room(5,"in an empty field", new ArrayList<NPC>(),new ArrayList<InanimateObjects>());
 
-        outside = new Room(1,"outside the main entrance of the university", new ArrayList<NPC>(),new ArrayList<InanimateObjects>());
-        theatre = new Room(2,"in a lecture theatre", new ArrayList<NPC>(),new ArrayList<InanimateObjects>());
-        pub = new Room(3,"in the campus pub", new ArrayList<NPC>(),new ArrayList<InanimateObjects>());
-        lab = new Room(4,"in a computing lab", new ArrayList<NPC>(),new ArrayList<InanimateObjects>());
-        office = new Room(5,"in the computing admin office", new ArrayList<NPC>(),new ArrayList<InanimateObjects>());
+        Village_center.setExit("east", Farm);
+        Village_center.setExit("south", Market);
+        Village_center.setExit("west", Home);
 
-        outside.setExit("east", theatre);
-        outside.setExit("south", lab);
-        outside.setExit("west", pub);
+        Farm.setExit("west", Village_center);
+        Farm.setExit("east", Field);
 
-        theatre.setExit("west", outside);
+        Home.setExit("east", Village_center);
 
-        pub.setExit("east", outside);
+        Market.setExit("north", Village_center);
 
-        lab.setExit("north", outside);
-        lab.setExit("east", office);
+        Field.setExit("west",Farm);
 
-        office.setExit("west", lab);
 
-        return outside;
+        return Village_center;
     }
 
 
