7ab28553dfe753e51a34a930b928e781febbab53
Storm551 <stormfrederik@gmail.com>
2021-10-26 12:17:23 +0200

    remove arraylist from inanimateobjects

--- 1.Iteration/src/InanimateObjects.java
+++ 1.Iteration/src/InanimateObjects.java
@@ -3,16 +3,10 @@ import java.util.ArrayList;
 public class InanimateObjects extends Interactables {
     private Item foundItem;
     private boolean isChecked;
-    private ArrayList<InanimateObjects> inanimateObjects;
 
     public InanimateObjects(String name, String shortDescription, String longDescription ,Item foundItem) {
         super(name, shortDescription, longDescription);
         this.foundItem = foundItem;
-        this.inanimateObjects = inanimateObjects;
-    }
-
-    public void addInanimateObject(InanimateObjects inanimateObject) {
-        this.inanimateObjects.add(inanimateObject);
     }
 
     public String interact(String[] interactString){
