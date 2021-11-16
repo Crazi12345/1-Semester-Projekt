9980db99ad2b949ad603f276f497d1ce6c996efa
Patrick <Patrickbjerre@outlook.dk>
2021-11-16 09:23:45 +0100

    Added gui support to main

--- 2.Iteration/src/main/java/domain/Main.java
+++ 2.Iteration/src/main/java/domain/Main.java
@@ -1,11 +1,22 @@
 package domain;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 
-public class Main {
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(game.adventures.Main.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("");
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public static void main(String[] args) {
-        Game s = new Game();
-        s.play();
-
+        launch();
     }
 }
